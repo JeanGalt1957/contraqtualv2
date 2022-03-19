44e000aebd26fc42315c3bf4b6ebeefb2b6f4679
Matthew Gilbertson <matthew.f.gilbertson@gmail.com>
2022-03-19 16:28:17 -0400

    v1.0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Welcome = (): React.ReactElement => {
         return (
             <>
                 <Head>
-                    <title>Welcome to funtec!</title>
+                    <title>Contraqtual</title>
                     <link rel="icon" type="image/x-icon" href="/images/appIcon.svg"></link>
                 </Head>
 
@@ -26,54 +26,21 @@ const Welcome = (): React.ReactElement => {
     const DesktopVersion = () => {
         return (
             <div className={`${styles.main} desktopView`}>
-                <div id={styles.navElements}>
-                    <div id={styles.navLeftSide}>
-                        <Link href={"/"}>
-                            <div id={styles.home} className={styles.navElement}>
-                                <Image src="/images/appIcon.svg" alt="App icon" width={30} height={30}></Image>
-                            </div>
-                        </Link>
-                        
-                        <Link href={"/polls"}>
-                            <div id={styles.polls} className={styles.navElement}>Enter app
-                                <div className={styles.navElementSurline}></div>
-                            </div>
-                        </Link>
-                        <div 
-                            id={styles.oracles} 
-                            className={styles.navElement}
-                            onClick={() => {window.open(metaData.docsUrl, "_blank")}}
-                        >User guide
-                            <div className={styles.navElementSurline}></div>
-                        </div>
-                        <div 
-                            id={styles.activePolls} 
-                            className={styles.navElement}
-                            onClick={() => {window.open(metaData.githubUrl, "_blank")}}
-                        >Explore source code
-                            <div className={styles.navElementSurline}></div>
-                        </div>
-                    </div>
-                </div>
+
                 <div id={styles.body}>
                     <div id={styles.firstView}>
                         <div id={styles.leftView}>
                             <div id={styles.leftViewHeader}>
-                                <h1 id={styles.title}>Welcome to Funtec!</h1>
-                                <p id={styles.subTitle}>Here we help you <span className={styles.boldText}>get the videos, streams, books that you want</span> by allowing you to incentivise creators to produce specific content.</p>
+                                <h1 id={styles.title}>Contraqtual</h1>
+                                <p id={styles.subTitle}> Predict, hedge, win</p>
                                 <div id={styles.firstViewButtons}>
                                     <Link href={"/polls"}>
                                         <input type={"button"} value={"Enter app"} id={styles.enterAppButton}></input>
                                     </Link>
-                                    <div onClick={() => {window.open(metaData.docsUrl, "_blank")}}>
-                                        <input type={"button"} value={"Learn how to use"}></input>
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div id={styles.firstViewSvg}>
-                            <img src="/images/welcomePageRigthImage.svg"></img>
-                        </div>
+
                     </div>
                 </div>
             </div>
@@ -84,15 +51,12 @@ const Welcome = (): React.ReactElement => {
         return (
             <div className={`${styles.bodyMobile} mobileView`}>
                     <div id={styles.firstViewMobile}>
-                        <h1 id={styles.titleMobile}>Welcome to Funtec!</h1>
-                        <p id={styles.subTitleMobile}>Here we help you <span className={styles.boldText}>get the videos, streams, books that you want</span> by allowing you to incentivise creators to produce specific content.</p>
+                        <h1 id={styles.titleMobile}>Contraqtual</h1>
+                        <p id={styles.subTitleMobile}>Predict, hedge, win</p>
                         <div id={styles.firstViewButtonsMobile}>
                             <Link href={"/polls"}>
                                 <input type={"button"} value={"Enter app"} id={styles.enterAppButton}></input>
                             </Link>             
-                            <div onClick={() => {window.open(metaData.docsUrl, "_blank")}}>
-                                <input type={"button"} value={"Learn how to use"}></input>
-                            </div>
                         </div>           
                     </div>
             </div>
